236a765b79c7d9a4f6870933e753a24adfbbd9eb
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-04-13 20:12:02 +0200

    w02d04

---
+++ day-04/Bunnies/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int bunny(int x);
+
+int main() {
+
+    int x = 10;
+    int ears = bunny(x);
+    std::cout << ears << std::endl;
+
+    return 0;
+}
+
+int bunny(int x) {
+    if (x == 1) {
+        return 2;
+    } else {
+        return bunny(x - 1) + 2;
+    }
+
+}
---
+++ day-04/Number adder/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int add(int n);
+
+int main()
+{
+    int n;
+
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
+
+    std::cout << "Sum =  " << add(n);
+
+    return 0;
+}
+
+int add(int n)
+{
+    if(n != 0)
+        return n + add(n - 1);
+    return 0;
+}
