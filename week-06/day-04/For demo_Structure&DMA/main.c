756d656441f16124d25d611dd3b1e3a70ff2df49
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-05-20 10:31:47 +0200

    w06d04

--- week-06/day-04/For demo_Structure&DMA/main.c
+++ week-06/day-04/For demo_Structure&DMA/main.c
@@ -1,5 +1,8 @@
-/*C program to read and print students' details
-using structure and Dynamic Memory Allocation.*/
+
+/*
+ *C program to read and print students' details
+ *using structure and Dynamic Memory Allocation
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +16,7 @@ struct student {
 
 int main()
 {
-    /*The entered details will be stored in this ***********/
+    /*The entered details will be stored in this pointer*/
     struct student *pointerOfStudents;
     int numberOfStudents;
     int i;
@@ -21,7 +24,7 @@ int main()
     printf("Enter total number of students:\n");
     scanf("%d", &numberOfStudents);
 
-    /*Allocate memory dynamically for numberOfStudents objects*/
+    /*Allocate memory dynamically for the amount equal to numberOfStudents objects*/
     pointerOfStudents = (struct student *) malloc(numberOfStudents * sizeof(struct student));
 
     /*Error handling*/
@@ -34,19 +37,32 @@ int main()
             printf("\nEnter details of student [%d]:\n", i + 1);
             printf("\n");
             printf("- Enter full name:\n");
-            scanf(" "); /*clear input buffer*/
+            /*Clear input buffer*/
+            scanf(" ");
+            /*The function gets() reads until new line (\n),
+             * meaning it can handle space, like a first and a last name together
+             */
             gets((pointerOfStudents + i)->fullName);
             printf("- Enter ID number:\n");
             scanf("%d", &(pointerOfStudents + i)->idNumber);
             printf("- Enter exam result:\n");
             scanf("%f", &(pointerOfStudents + i)->examResult);
+            /*In each loop the details will be added to the pointer*/
         }
 
-        printf("\nEntered details are:\n");
+        printf("\nThe entered details are:\n");
         for (i = 0; i < numberOfStudents; i++) {
+            /*
+             * %.2f format specifier print only the first 2 digits of the floating point data type
+             * after the point (so it will reduce the precision to 2 digits);
+             * also with \t the details list will be neat
+             */
             printf("%s \t %d \t %.2f\n", (pointerOfStudents + i)->fullName,
                    (pointerOfStudents + i)->idNumber, (pointerOfStudents + i)->examResult);
         }
     }
+    /*Deallocate*/
+    free(pointerOfStudents);
+    pointerOfStudents = NULL;
     return 0;
 }
