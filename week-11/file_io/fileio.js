83a49eb4828b7433604244475e2f15fc60031b3d
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-06-19 18:54:38 +0200

    file io

--- week-11/file_io/fileio.js
+++ week-11/file_io/fileio.js
@@ -1,4 +1,4 @@
 var fs = require('fs'); // we require modul, var name = module name
 var read = fs.readFileSync('read.txt', 'utf8');
-//console.log(read);
+// console.log(read);
 fs.writeFileSync('write.txt', read);
