40c62087eb182cabafb785ab27d944c17054e242
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-06-19 18:52:16 +0200

    typescript /objects

--- week-11/ts_objects/demo3.ts
+++ week-11/ts_objects/demo3.ts
@@ -1,11 +1,11 @@
-/*var pets ={
+/* var pets ={
 animaltype:"cica",
 animalage:4
 }
 
 document.write("My pet: "+pets.animaltype)
 document.write("<br/>")
-document.write("Age of my pet: "+pets.animalage)*/
+document.write("Age of my pet: "+pets.animalage) */
 
 let john = {"name": "john", "speak": function(){console.log("speaking");}};
 console.log(typeof john);
