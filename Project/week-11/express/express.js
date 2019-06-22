7a66b4a543e9421f4dda679ef697bdf4f4314c85
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-06-23 00:12:00 +0200

    Express

---
+++ Project/week-11/express/express.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile( __dirname + '/index.html');
+});
+
+app.get('/contact', function(req, res){
+    res.sendFile( __dirname + '/contact.html');
+});
+
+app.listen(3000);
