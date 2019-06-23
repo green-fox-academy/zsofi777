d57b70809695fd03b99681a4746355c9a9f869d3
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-06-23 16:11:58 +0200

    Modified temp.js & profile.ejs

--- Project/week-11/templating/temp.js
+++ Project/week-11/templating/temp.js
@@ -13,7 +13,7 @@ app.get('/contact', function(req, res){
 });
 
 app.get('/profile/:name', function(req, res){
-    var data = {age: 29, job: 'sales manager'};
+    var data = {age: 29, job: 'sales manager', hobbies: ['netflix', 'reading', 'running']};
     res.render('profile', {person: req.params.name, data: data});
 });
 
